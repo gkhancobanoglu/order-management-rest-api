f04b872835fba6cd6c9283199e39dff6d3a46746
Gökhan Çobanoğlu <gkhancobanoglu@gmail.com>
2025-03-16 20:00:48 +0300

    Initial commit

---
+++ src/test/java/com/cobanoglu/enocatask/EnocaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobanoglu.enocatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnocaTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
